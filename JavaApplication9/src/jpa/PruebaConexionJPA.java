152a94d37a5d97b3c97e023401ebcc05853b662a
Lesly Polo <128642999+leslypoloascue@users.noreply.github.com>
2023-07-05 16:41:42 -0500

    agenda JPA

---
+++ JavaApplication9/src/jpa/PruebaConexionJPA.java
@@ -0,0 +1,28 @@
+package jpa;
+
+import ch.makery.address.model.Person;
+
+
+public class PruebaConexionJPA {
+
+    public static void main(String[] args) {
+        jpaPerson jpa = new jpaPerson ();
+
+        // Insertar un nuevo estudiante
+        Person nuevoPerson = new Person();
+        nuevoPerson.setFirstName("lesly");
+        nuevoPerson.setLastName("Polo");
+        nuevoPerson.setStreet("san jeronimo");
+        nuevoPerson.setpostalCode("421");
+        nuevoPerson.setCity("calle hospital");
+        nuevoPerson.setBirthday("21.02,199");
+
+
+        jpa.insertapersona(nuevoPerson);
+
+        // Cargar y mostrar toda la agenda de las persona en la base de datos
+        jpa.cargarpersona().forEach(Person -> {
+            System.out.println(Person.getFirstName() + " " + Person.getFirstName() + " " + Person.getStreet()  + " "  +Person.getpostalCode() +" " +Person.getCity() + " " +Person.getBirthday()); 
+        });
+    }
+}
